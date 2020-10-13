e2ed069c65b9c6f8c155e06e57f9a7a29fae530b
OmorFarukRakib <rakib1001@gmail.com>
2020-10-13 14:03:10 +0600

    Updates

---
+++ precache-manifest.540214b1630b0328fe2c2de9ea6a4daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4116fc665adb44f86135f0eeb21e4a6",
+    "url": "/Test-React-App-Deploy/index.html"
+  },
+  {
+    "revision": "848a78a61c53017813e8",
+    "url": "/Test-React-App-Deploy/static/css/main.b59f09d2.chunk.css"
+  },
+  {
+    "revision": "c857a369db20d6a54197",
+    "url": "/Test-React-App-Deploy/static/js/2.2e504344.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Test-React-App-Deploy/static/js/2.2e504344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848a78a61c53017813e8",
+    "url": "/Test-React-App-Deploy/static/js/main.64c364eb.chunk.js"
+  },
+  {
+    "revision": "6f1c6c1d235eab166c41",
+    "url": "/Test-React-App-Deploy/static/js/runtime-main.421de41d.js"
+  }
+]);
